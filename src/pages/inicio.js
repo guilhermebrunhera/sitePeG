d1dceace7faa1579a184f4d50c5d703c45bc0479
Guilherme Brunhera <guilhermecbrunhera@gmail.com>
2023-02-25 18:51:36 -0300

    Tela de inicio sendo construida 2.1

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -9,13 +9,13 @@ function Inicio() {
 
         <span>"Minha querida namorada,<br /><br />
 
-Ao longo de todas as nossas aventuras, sempre houve algo especial entre nós. Nós nos conhecemos há tanto tempo que, às vezes, parece que já percorremos um caminho tão longo juntos. Mas, por mais que tenhamos tido altos e baixos em nossas vidas, eu sempre soube que você era alguém especial.<br />
+Ao longo de todas as nossas aventuras, sempre houve algo especial entre nós. Nós nos conhecemos há tanto tempo que, às vezes, parece que já percorremos um caminho tão longo juntos. Mas, por mais que tenhamos tido altos e baixos em nossas vidas, eu sempre soube que você era alguém especial.<br /><br />
 
-Quando penso em você, lembro-me de todas as coisas que me fazem sentir feliz. Seu sorriso brilhante, sua risada contagiante e sua gentileza incansável. Cada pequeno gesto que você faz me faz sentir mais amado e mais afortunado de ter você em minha vida.<br />
+Quando penso em você, lembro-me de todas as coisas que me fazem sentir feliz. Seu sorriso brilhante, sua risada contagiante e sua gentileza incansável. Cada pequeno gesto que você faz me faz sentir mais amado e mais afortunado de ter você em minha vida.<br /><br />
 
-O que mais me encanta é a nossa história juntos. Nós não começamos nossa jornada como um casal comum. Em vez disso, nossos caminhos se cruzaram várias vezes ao longo dos anos, e eu sempre soube que havia algo mais profundo entre nós.<br />
+O que mais me encanta é a nossa história juntos. Nós não começamos nossa jornada como um casal comum. Em vez disso, nossos caminhos se cruzaram várias vezes ao longo dos anos, e eu sempre soube que havia algo mais profundo entre nós.<br /><br />
 
-Quando finalmente encontramos o caminho um para o outro, senti como se o universo estivesse alinhado perfeitamente. E agora, aqui estamos, juntos e apaixonados, celebrando cada momento que passamos juntos.<br />
+Quando finalmente encontramos o caminho um para o outro, senti como se o universo estivesse alinhado perfeitamente. E agora, aqui estamos, juntos e apaixonados, celebrando cada momento que passamos juntos.<br /><br />
 
 Minha querida, saiba que eu sempre serei grato por cada momento que compartilhamos. Eu sempre serei o seu companheiro de aventuras, o seu confidente, o seu apoio inabalável. Eu amo você mais do que qualquer palavra pode expressar, e mal posso esperar para ver onde nossa história juntos nos levará a seguir.<br /><br />
 
