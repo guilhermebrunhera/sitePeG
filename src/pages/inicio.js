6ff7471f5cb1ee105bf35ccc526628bde8af2f74
Guilherme Brunhera <guilhermecbrunhera@gmail.com>
2023-02-25 18:49:58 -0300

    Tela de inicio sendo construida 2.0

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -7,7 +7,21 @@ function Inicio() {
         <span className='titleInicio'>Carta aberta ao meu amor</span><br />
         <span className='titleInicioMini'>1 Mês de namoro <img src="../favicon-16x16.png" alt='heart'></img></span><br />
 
-        <span></span>
+        <span>"Minha querida namorada,<br /><br />
+
+Ao longo de todas as nossas aventuras, sempre houve algo especial entre nós. Nós nos conhecemos há tanto tempo que, às vezes, parece que já percorremos um caminho tão longo juntos. Mas, por mais que tenhamos tido altos e baixos em nossas vidas, eu sempre soube que você era alguém especial.<br />
+
+Quando penso em você, lembro-me de todas as coisas que me fazem sentir feliz. Seu sorriso brilhante, sua risada contagiante e sua gentileza incansável. Cada pequeno gesto que você faz me faz sentir mais amado e mais afortunado de ter você em minha vida.<br />
+
+O que mais me encanta é a nossa história juntos. Nós não começamos nossa jornada como um casal comum. Em vez disso, nossos caminhos se cruzaram várias vezes ao longo dos anos, e eu sempre soube que havia algo mais profundo entre nós.<br />
+
+Quando finalmente encontramos o caminho um para o outro, senti como se o universo estivesse alinhado perfeitamente. E agora, aqui estamos, juntos e apaixonados, celebrando cada momento que passamos juntos.<br />
+
+Minha querida, saiba que eu sempre serei grato por cada momento que compartilhamos. Eu sempre serei o seu companheiro de aventuras, o seu confidente, o seu apoio inabalável. Eu amo você mais do que qualquer palavra pode expressar, e mal posso esperar para ver onde nossa história juntos nos levará a seguir.<br /><br />
+
+Com todo meu amor,
+
+Ass. Guilherme Cintra Brunhera, seu namorado."</span>
       </div>
     </div>
   );
